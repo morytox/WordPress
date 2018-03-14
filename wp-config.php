825276c946b8d386e6afa273211e43fcd0135d79
Felix Knoepke <morytox@hotmail.de>
2018-03-14 10:06:36 +0800

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -87,3 +87,6 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+       
+/** Sets up WordPress to use direct downloads instead of FTP */
+define('FS_METHOD','direct');
